bccf1a8ef1eb96c39c537e68ad531307ef68edca
ghulamrabbani883 <ghulamrabbani883@gmail.com>
2022-06-11 23:08:31 +0530

    updated word  count

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const charVal = document.getElementById("textarea");
 let totalChar = document.getElementById("total-char");
 let remainingChar = document.getElementById("remaining-char");
+let WordCounter = document.getElementById("total-word");
 
-let userChar = 0;
+let userChar = 0;//character count
+let splitChar = 0;//spliting character to count word
+let totalWOrd = 0;//word count
 const updateChar = () => {
+  splitChar = charVal.value;
   userChar = charVal.value.length;
   totalChar.innerText = userChar;
   remainingChar.innerText = 150 - userChar;
+  totalWOrd = splitChar.split(" ");
+  WordCounter.innerText = totalWOrd.length;
+  console.log(totalWOrd);
   if (userChar >= 150 || remainingChar <= 0) {
     alert("Cant exceed 150 character");
   }
